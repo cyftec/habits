71b3a5c2a5c314b346687535189b042c95ebcc8d
chnkrydv <chnkrydv@gmail.com>
2025-04-29 00:07:30 +0530

    build: generate build for prod release

--- karma.ts
+++ karma.ts
@@ -11,7 +11,7 @@ export const projectFileNames: ProjectFileNames = {
   },
   generated: {
     stagingDir: "stage",
-    publishDir: "prod",
+    publishDir: "docs",
     bunLockFile: "bun.lock",
     bunLockBFile: "bun.lockb",
     gitIgnoreFile: ".gitignore",
