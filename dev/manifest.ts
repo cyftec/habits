369d59ec16d6c75bd467fb9358874a4d9824b6cb
chnkrydv <chnkrydv@gmail.com>
2025-05-12 18:40:55 +0530

    feat: add screenshots for installer, make background color lighter

--- dev/manifest.ts
+++ dev/manifest.ts
@@ -1,26 +1,36 @@
 import { WebAppManifest } from "web-app-manifest";
 
 const assetsPath = "/assets";
-const imagesPath = `${assetsPath}/images`;
+const manifestImagesPath = `${assetsPath}/images/manifest-only`;
 
 const manifest: WebAppManifest = {
   short_name: "Habits",
   name: "Habits (by Cyfer)",
   icons: [
     {
-      src: `${imagesPath}/192_logo.png`,
+      src: `${manifestImagesPath}/192_logo.png`,
       sizes: "192x192",
       type: "image/png",
     },
     {
-      src: `${imagesPath}/512_logo.png`,
+      src: `${manifestImagesPath}/512_logo.png`,
       sizes: "512x512",
       type: "image/png",
     },
   ],
   screenshots: [
     {
-      src: `${imagesPath}/screenshot.png`,
+      src: `${manifestImagesPath}/screenshot1.png`,
+      sizes: "640x320",
+      type: "image/png",
+    },
+    {
+      src: `${manifestImagesPath}/screenshot2.png`,
+      sizes: "640x320",
+      type: "image/png",
+    },
+    {
+      src: `${manifestImagesPath}/screenshot3.png`,
       sizes: "640x320",
       type: "image/png",
     },
@@ -30,7 +40,7 @@ const manifest: WebAppManifest = {
   display: "standalone",
   orientation: "portrait",
   theme_color: "#e87b52",
-  background_color: "#f3cbbc",
+  background_color: "#fff0eb",
 };
 
 export default manifest;
